9c22d674638b9047127ca1449caf92921907cb32
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-09-09 16:06:16 +0900

    動作するようになったので追加する

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from willer!")
+def nth_lucky_number(n: int) -> int:
+    """
+    4と7のみからなる10桁の整数のうち、N番目のものを返す。
+    (memo 4を0, 7を1と捉えると2進数で考えられる)
+
+    @param n: 1から始まる整数
+    """
+    digits = []
+    n -= 1  # 0-indexedにする
+    for _ in range(10):
+        digits.append("4" if n % 2 == 0 else "7")
+        n //= 2
+    return int("".join(reversed(digits)))
 
 
 if __name__ == "__main__":
-    main()
+    N = int(input())
+    print(nth_lucky_number(N))
